31a36c191af5239daf001e68500f255759fa4b47
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2021-06-30 09:38:46 +0100

    Delete redundant code

--- app/models/errors/mtdErrors.scala
+++ app/models/errors/mtdErrors.scala
@@ -16,48 +16,26 @@
 
 package models.errors
 
-import play.api.libs.json.{Json, Writes}
+import play.api.libs.json.{Json, OWrites}
 
-case class MtdError(code: String, message: String, paths: Option[Seq[String]] = None)
+case class MtdError(code: String, message: String)
+
 
 object MtdError {
-  implicit val writes: Writes[MtdError] = Json.writes[MtdError]
+  implicit val writes: OWrites[MtdError] = Json.writes[MtdError]
+
+  implicit def genericWrites[T <: MtdError]: OWrites[T] =
+    writes.contramap[T](c => c: MtdError)
 }
 
 object NinoFormatError extends MtdError("FORMAT_NINO", "The provided NINO is invalid")
 object TaxYearFormatError extends MtdError("FORMAT_TAX_YEAR", "The provided tax year is invalid")
 
-// Rule Errors
-object RuleTaxYearNotSupportedError extends MtdError(
-  code = "RULE_TAX_YEAR_NOT_SUPPORTED",
-  message = "Tax year not supported, because it precedes the earliest allowable tax year"
-)
-
-object RuleTaxYearRangeInvalidError extends MtdError(
-  code = "RULE_TAX_YEAR_RANGE_INVALID",
-  message = "Tax year range invalid. A tax year range of one year is required"
-)
-
-object RuleIncorrectOrEmptyBodyError extends MtdError("RULE_INCORRECT_OR_EMPTY_BODY_SUBMITTED", "An empty or non-matching body was submitted")
-
 //Standard Errors
-object NotFoundError extends MtdError("MATCHING_RESOURCE_NOT_FOUND", "Matching resource not found")
-
 object DownstreamError extends MtdError("INTERNAL_SERVER_ERROR", "An internal server error occurred")
 
 object BadRequestError extends MtdError("INVALID_REQUEST", "Invalid request")
 
-object BVRError extends MtdError("BUSINESS_ERROR", "Business validation error")
-
-object ServiceUnavailableError extends MtdError("SERVICE_UNAVAILABLE", "Internal server error")
-
 //Authorisation Errors
 object UnauthorisedError extends MtdError("CLIENT_OR_AGENT_NOT_AUTHORISED", "The client and/or agent is not authorised")
 object InvalidBearerTokenError extends MtdError("UNAUTHORIZED", "Bearer token is missing or not authorized")
-
-// Accept header Errors
-object  InvalidAcceptHeaderError extends MtdError("ACCEPT_HEADER_INVALID", "The accept header is missing or invalid")
-
-object  UnsupportedVersionError extends MtdError("NOT_FOUND", "The requested resource could not be found")
-
-object InvalidBodyTypeError extends MtdError("INVALID_BODY_TYPE", "Expecting text/json or application/json body")
