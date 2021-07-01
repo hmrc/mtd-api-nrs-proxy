60ec0b389292e32dee99f9285c36f4ea3b9738fb
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2021-07-01 14:08:46 +0100

    Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,20 +22,18 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "3.4.0",
-    "uk.gov.hmrc"   %% "domain"            % "5.11.0-play-27",
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % "5.4.0",
     "org.typelevel" %% "cats-core"         % "2.3.0",
     "com.chuusai"   %% "shapeless"         % "2.4.0-M1",
-    "com.typesafe.play" %% "play-json-joda"    % "2.6.0"
+    "com.typesafe.play" %% "play-json-joda"    % "2.9.2"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"                % "3.2.3"             % scope,
+    "org.scalatest"          %% "scalatest"                % "3.2.9"             % scope,
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.36.8"            % scope,
-    "org.scalacheck"         %% "scalacheck"               % "1.15.1"            % scope,
     "org.scalamock"          %% "scalamock"                % "5.1.0"             % scope,
     "com.typesafe.play"      %% "play-test"                % PlayVersion.current % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "4.0.3"             % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"             % scope,
     "com.github.tomakehurst" % "wiremock-jre8"             % "2.27.2"            % scope,
     "com.miguno.akka"        %% "akka-mock-scheduler"      % "0.5.5"             % scope
   )
